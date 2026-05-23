5056442b752d63baa65fc385375e5a1ae6b7c0dc
HEMLOKapx <gobou0718@gmail.com>
2026-05-23 19:50:26 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+data = {}
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form.get("name")
+        date = request.form.get("date")
+
+        if date not in data:
+            data[date] = []
+
+        data[date].append(name)
+
+    return render_template("index.html", data=data)
+
+app.run(host="0.0.0.0", port=5000)
+

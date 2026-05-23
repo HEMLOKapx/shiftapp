ba9fe7af7e84e010cdb6b3a7c9d76433835be65c
HEMLOKapx <gobou0718@gmail.com>
2026-05-23 20:14:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def index():
 
     return render_template("index.html", data=data)
 
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 ``
-
